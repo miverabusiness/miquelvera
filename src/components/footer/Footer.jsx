4d46f84da676654763b5e4ecc8dc2e25a064e969
miverabusiness <miverabusiness@gmail.com>
2023-07-07 02:15:11 +0200

    Footer responsive

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,57 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
+import { FaFacebookF } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
+import { IoLogoTwitter } from "react-icons/io";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+	return (
+		<footer>
+			<a href="#" className="footer__logo">
+				Miquel Vera
+			</a>
 
-export default Footer
+			<ul className="permalinks">
+				<li>
+					<a href="#">Home</a>
+				</li>
+				<li>
+					<a href="#about">About</a>
+				</li>
+				<li>
+					<a href="#experience">Experience</a>
+				</li>
+				<li>
+					<a href="#services">Services</a>
+				</li>
+				<li>
+					<a href="#portfolio">Portfolio</a>
+				</li>
+				<li>
+					<a href="#testimonials">Testimonials</a>
+				</li>
+				<li>
+					<a href="#contacts">Contact</a>
+				</li>
+			</ul>
+
+			<div className="footer__socials">
+				<a href="https://facebook.com">
+					<FaFacebookF className="footer_socals-icon" />
+				</a>
+				<a href="https://instagram.com">
+					<FiInstagram className="footer__socials-icon" />
+				</a>
+				<a href="https://twitter.com">
+					<IoLogoTwitter className="footer__socials-icon" />
+				</a>
+			</div>
+
+			<div className="footer__copyright">
+				<small>&copy; Miquel Vera. All rights reserved.</small>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
